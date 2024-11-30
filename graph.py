54572da85174746069fbd92050cdcb8b9b5d3f6b
Spike756 <145921536+Spike756@users.noreply.github.com>
2024-11-30 12:41:26 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,1 +1,14 @@
-
+class Graph:
+    def __init__(self):
+        self.adjacency_list = {}
+    
+    def add_node(self, node):
+        if node not in self.adjacency_list:
+            self.adjacency_list[node] = []
+    
+    def add_edge(self, start, end, weight=1):
+        self.adjacency_list[start].append((end, weight))
+        self.adjacency_list[end].append((start, weight))  # For undirected graph
+    
+    def get_neighbors(self, node):
+        return self.adjacency_list.get(node, [])
