ef444972c97d8ab44c6faf2150dd8e8eec70979a
Jonathan Emons <145884781+ja1emons@users.noreply.github.com>
2024-12-03 03:42:38 -0500

    Update graph.py
    Adding error handling.

--- graph.py
+++ graph.py
@@ -7,8 +7,12 @@ class Graph:
             self.adjacency_list[node] = []
     
     def add_edge(self, start, end, weight=1):
+        if start in not self.adjacency_list or end not in self.adjcency_list:
+            raise ValueError(f"Both sides '{start}' and '{end}' must exist before adding an edge.")
         self.adjacency_list[start].append((end, weight))
         self.adjacency_list[end].append((start, weight))  # For undirected graph
     
     def get_neighbors(self, node):
+        if node not in self.adjacency_list:
+            raise KeyError(f"Node '{node}' does not exist in graph.")
         return self.adjacency_list.get(node, [])
